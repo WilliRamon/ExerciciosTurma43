7f17f5c46b7b89857131ae95da5f0d14df3e628d
WilliRamon <acdc.ramon@hotmail.com>
2021-12-08 23:04:04 -0300

    Adicionando exercício 06

---
+++ Java-lista01-inicio/src/lista01/Java01Exer07.java
@@ -0,0 +1,32 @@
+package lista01;
+
+import java.util.Scanner;
+
+public class Java01Exer07 {
+
+	public static void main(String[] args) {
+		float a, b, c, d, e, f, x, y;
+		Scanner ler = new Scanner(System.in);
+		System.out.println("Para encontrar os valores de x e y, digite o valores pedidos");
+		System.out.print("Digite o valor de A: ");
+		a = ler.nextInt();
+		System.out.print("Digite o valor de B: ");
+		b = ler.nextInt();
+		System.out.print("Digite o valor de C: ");
+		c = ler.nextInt();
+		System.out.print("Digite o valor de D: ");
+		d = ler.nextInt();
+		System.out.print("Digite o valor de E: ");
+		e = ler.nextInt();
+		System.out.print("Digite o valor de F: ");
+		f = ler.nextInt();
+		
+		x = (c*e - b*f) / (a*e - b*d);
+		y = (a*f - c*d) / (a*e - b*d);
+		
+		System.out.println("Valor de x é: " + x);
+		System.out.println("Valor de y é: " + y);
+
+	}
+
+}
