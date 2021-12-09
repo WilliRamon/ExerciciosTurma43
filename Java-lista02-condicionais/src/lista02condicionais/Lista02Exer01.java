541c0e5ee543e2d7adfcdcfea675b6ffebe91a96
WilliRamon <acdc.ramon@hotmail.com>
2021-12-09 12:45:35 -0300

    Criando o segundo exercício

--- Java-lista02-condicionais/src/lista02condicionais/Lista02Exer01.java
+++ Java-lista02-condicionais/src/lista02condicionais/Lista02Exer01.java
@@ -1,9 +1,27 @@
 package lista02condicionais;
 
+import java.util.Scanner;
+
 public class Lista02Exer01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner ler = new Scanner(System.in);
+		int n1, n2, n3;
+		System.out.println("Projeto para descobrir qual numero é maior!");
+		System.out.print("Digite o primeiro numero: ");
+		n1 = ler.nextInt();
+		System.out.print("Digite o segundo numero: ");
+		n2 = ler.nextInt();
+		System.out.print("Digite o terceiro numero: ");
+		n3 = ler.nextInt();
+		
+		if((n1 > n2) && (n1 > n3)) {
+			System.out.println("O maior numero é o " + n1);
+		}else if(n2 > n3) {
+			System.out.println("O maior numero é o " + n2);
+		}else {
+			System.out.println("O maior numero é o " + n3);
+		}
 
 	}
 
