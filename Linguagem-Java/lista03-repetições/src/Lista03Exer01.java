aa86f5ff3541c348a9b0e73b4f29093a4c812e18
WilliRamon <acdc.ramon@hotmail.com>
2021-12-10 12:39:38 -0300

    Criando primeiro exercício

---
+++ Linguagem-Java/lista03-repetições/src/Lista03Exer01.java
@@ -0,0 +1,14 @@
+
+public class Lista03Exer01 {
+
+	public static void main(String[] args) {
+		int x;
+		for(x = 1000; x <= 1999; x++) {
+			if(x % 11 == 5) {
+				System.out.println(x);
+			}
+		}
+
+	}
+
+}
