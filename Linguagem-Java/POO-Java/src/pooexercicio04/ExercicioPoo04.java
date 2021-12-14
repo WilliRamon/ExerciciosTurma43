8c0c2a1148c6144ebecdc593e49249fca60ed87f
WilliRamon <acdc.ramon@hotmail.com>
2021-12-14 18:07:16 -0300

    Criando exercício 04

---
+++ Linguagem-Java/POO-Java/src/pooexercicio04/ExercicioPoo04.java
@@ -0,0 +1,10 @@
+package pooexercicio04;
+
+public class ExercicioPoo04 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Linguagem-Java/POO-Java/src/pooexercicio04/Funcionario.java
@@ -0,0 +1,15 @@
+package pooexercicio04;
+
+public class Funcionario {
+
+	private String nome;
+	private String cargo;
+	private String area;
+	private int idade;
+	private boolean ferias;
+	
+	
+	public void status() {
+		System.out.println("");
+	}
+}
