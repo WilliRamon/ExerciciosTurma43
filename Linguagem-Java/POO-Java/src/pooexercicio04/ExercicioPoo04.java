edc5ace71e0b54674f6ebc86a207b60d75a7d8b0
WilliRamon <acdc.ramon@hotmail.com>
2021-12-14 21:16:28 -0300

    Adicionando exercício 04

--- Linguagem-Java/POO-Java/src/pooexercicio04/ExercicioPoo04.java
+++ Linguagem-Java/POO-Java/src/pooexercicio04/ExercicioPoo04.java
@@ -3,7 +3,14 @@ package pooexercicio04;
 public class ExercicioPoo04 {
 
 	public static void main(String[] args) {
+		Funcionario f1 = new Funcionario("Willi");
 		
+		f1.setCargo("Desenvolvedor Java - jr");
+		f1.setArea("TI");
+		f1.setIdade(26);
+		f1.sairFerias();
+		
+		f1.status();
 
 	}
 
--- Linguagem-Java/POO-Java/src/pooexercicio04/Funcionario.java
+++ Linguagem-Java/POO-Java/src/pooexercicio04/Funcionario.java
@@ -8,8 +8,97 @@ public class Funcionario {
 	private int idade;
 	private boolean ferias;
 	
+	public Funcionario(String nome) {
+		this.setNome(nome);
+	}
+	
+	
+	public void entrarFerias() {
+		this.ferias = true;
+	}
+	public void sairFerias() {
+		this.ferias = false;
+	}
 	
 	public void status() {
-		System.out.println("");
+		System.out.println("Dados Cadastrais do Funcionário: ");
+		System.out.println("Nome: " + this.getNome());
+		System.out.println("Cargo: " + this.getCargo());
+		System.out.println("Área: " + this.getArea());
+		System.out.println("Idade: " + this.getIdade());
+		System.out.println("Está de férias? " + this.getFerias());
 	}
+	
+	
+	public String getNome() {
+		return nome;
+	}
+
+
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+
+	public String getCargo() {
+		return cargo;
+	}
+
+
+
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+
+
+
+	public String getArea() {
+		return area;
+	}
+
+
+
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+
+
+
+	public int getIdade() {
+		return idade;
+	}
+
+
+
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+
+
+
+	public boolean getFerias() {
+		return ferias;
+	}
+
+
+
+
+	public void setFerias(boolean ferias) {
+		this.ferias = ferias;
+	}
+
+
+
+
+
 }
